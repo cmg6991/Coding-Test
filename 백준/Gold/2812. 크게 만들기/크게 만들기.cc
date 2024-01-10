439f3ed6f50689a23572d96102860fc00c3deb5a
cmg6991 <110959882+cmg6991@users.noreply.github.com>
2024-01-10 22:52:16 +0900

    [Gold III] Title: 크게 만들기, Time: 28 ms, Memory: 3544 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int N, K, i = 0;
+	string num;
+	vector<char> v;
+	cin >> N >> K;
+	cin >> num;
+
+	while (i != num.size())
+	{
+		while (K != 0 && !v.empty() && v.back() < num[i])
+		{
+			v.pop_back();
+			K--;
+		}
+		v.push_back(num[i]);
+		i++;
+	}
+	while (K--)
+		v.pop_back();
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i];
+}
